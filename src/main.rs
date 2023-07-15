e833178d15747af41b57a77881ada0ab5da43e24
CallMePierson <alisonepierson@outlook.com>
2023-07-15 17:30:16 -0400

    hello world router

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 #![allow(unused)]
 
+use std::net::SocketAddr;
+
+use axum::response::Html;
 use axum::Router;
 use axum::routing::get;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let routes_start = Router::new().route(
+        "/start",
+        get(|| async { Html("Hello World!!")}),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!(">> LISTENING on http://{addr}\n");
+
+    axum::Server::bind(&addr).serve(routes_start.into_make_service()).await.unwrap();
 }
