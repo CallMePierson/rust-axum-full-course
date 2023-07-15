8bf6d05efbcc5b6aad079d7cab549c250211c784
CallMePierson <alisonepierson@outlook.com>
2023-07-15 18:03:03 -0400

    Added tests and notes

---
+++ examples/quick_test.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let hc = httpc_test::new_client("http:127.0.0.1:8080")?;
+
+    hc.do_get("/start").await?.print().await?;
+
+    Ok(())
+}
