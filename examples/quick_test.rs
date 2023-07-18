4ef381f34212137eb198476c63da5b866dc43cd1
CallMePierson <alisonepierson@outlook.com>
2023-07-18 17:40:37 -0400

    Added second route hello/name

--- examples/quick_test.rs
+++ examples/quick_test.rs
@@ -7,6 +7,7 @@ async fn main() -> Result<()> {
     let hc = httpc_test::new_client("http:127.0.0.1:8080")?;
 
     hc.do_get("/start?name=Jenn").await?.print().await?;
+    hc.do_get("/hello/Mike").await?.print().await?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::net::SocketAddr;
 
-use axum::extract::Query;
+use axum::extract::{Query, Path};
 use axum::response::{Html, IntoResponse};
 use axum::Router;
 use axum::routing::get;
@@ -13,6 +13,9 @@ async fn main() {
     let routes_start = Router::new().route(
         "/start",
         get(handler_start),
+    ).route(
+        "/hello/:name",
+        get(handler_hello)
     );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -30,5 +33,11 @@ async fn handler_start(Query(params): Query<StartParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_start - {params:?}", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("Welcome {name}!!"))
+}
+
+async fn handler_hello(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {name:?}", "HANDLER");
+
     Html(format!("Hello {name}!!"))
 }
